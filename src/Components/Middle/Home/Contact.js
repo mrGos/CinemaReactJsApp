1a7770a2a7956db5d144a868e41a163377d0f73e
mrGos <hoangkhang0235@gmail.com>
2019-07-19 11:43:09 +0700

    add routing

---
+++ src/Components/Middle/Home/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div className="row search-container ml-3 mt-3">
+        <h2>Nhóm 9: </h2> <br></br>
+        <h5>Trương Hoàng Khang - 16520570</h5>
+      </div>
+    )
+  }
+}
